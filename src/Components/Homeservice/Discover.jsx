c526e85cc5d49a58045f2e9f963f1043b9765f82
sabbir-sabbir <iosabbir7890@gmail.com>
2024-12-18 16:10:05 +0600

    updaded

--- src/Components/Homeservice/Discover.jsx
+++ src/Components/Homeservice/Discover.jsx
@@ -1,18 +1,27 @@
-import React from 'react'
+import React from "react";
 
 const Discover = () => {
   return (
-    <section className='container mx-auto w-full h-auto p-2 mb-5 '>
-        <div>
-            <h1 className='font-lexend text-HB text-2xl text-start p-3 font-bold '>Also Discover</h1>
-        </div>
-        <div className='flex items-center gap-4'>
-           <div><img  src="discover1.png" alt="" /></div>
-           <div><img  src="discover2.png" alt="" /></div>
-           <div><img src="discover3.png" alt="" /></div>
-        </div>
-    </section>
-  )
-}
+    <section className="container mx-auto w-full h-auto p-2 mb-5 ">
+      <div>
+        <h1 className="font-lexend text-HB text-2xl text-start p-3 font-bold ">
+          Also Discover
+        </h1>
+      </div>
 
-export default Discover
+      <div className="flex items-center gap-4">
+        <div>
+          <img src="discover1.png" alt="" />
+        </div>
+        <div>
+          <img src="discover2.png" alt="" />
+        </div>
+        <div>
+          <img src="discover3.png" alt="" />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Discover;
